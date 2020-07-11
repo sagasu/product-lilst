f6c2b0c1ca32bfee506f4b4f6b40351c1d757d98
Mateusz Kopij <sagasu@gmail.com>
2020-07-11 13:56:21 +0100

    Updates

---
+++ precache-manifest.955824514fbda7ee7b6c48cdc2a6e61b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "939823fb8a9a3a48f7fecd87cde3ac20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeab660f6ae4c01ee825",
+    "url": "/static/css/main.f5f892e1.chunk.css"
+  },
+  {
+    "revision": "9e5ffe6fa170105d7366",
+    "url": "/static/js/2.6c3bbcb6.chunk.js"
+  },
+  {
+    "revision": "aeab660f6ae4c01ee825",
+    "url": "/static/js/main.dae868b6.chunk.js"
+  },
+  {
+    "revision": "e1374bf9d73870338873",
+    "url": "/static/js/runtime~main.2c7233da.js"
+  },
+  {
+    "revision": "669bf559c8807da8266dd9a6f556d622",
+    "url": "/static/media/OpenSans-Regular.669bf559.woff2"
+  }
+]);
